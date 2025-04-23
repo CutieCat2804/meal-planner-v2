b46f4eb4ca880c0c32e959e1df47e4ae4c39b9c4
CutieCat2804 <cutiecattv@gmail.com>
2025-04-23 21:02:33 +0200

    add robots

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,18 @@ import { trpc } from "@/utils/trpc";
 import { type AppType } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import system from "@/theme/theme";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider value={system}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <meta name="robots" content="noindex,nofollow" />
+      </Head>
+      <ChakraProvider value={system}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 };
 
